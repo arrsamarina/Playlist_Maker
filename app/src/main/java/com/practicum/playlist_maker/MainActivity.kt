f3aed6bcdb3abcb534d55f8430927b5dc06ae2c8
arina <arrsamarina@yandex.ru>
2024-04-07 18:12:58 +0300

    Init commit

--- app/src/main/java/com/practicum/playlist_maker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlist_maker/MainActivity.kt
@@ -4,8 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
-import android.view.View
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +26,5 @@ class MainActivity : AppCompatActivity() {
             val intentSettings = Intent(this, SettingsActivity::class.java)
             startActivity(intentSettings)
         }
-
     }
 }
